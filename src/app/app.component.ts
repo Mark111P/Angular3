966284847cc3ee76048909c6c713c86cd0123e0f
Mark111P <markusperevezentsev@gmail.com>
2024-06-21 16:34:15 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, INJECTOR } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular';
+  
+  info: Array<Info> =
+  [
+    new Info('Car','Some info about the biggest Car'),
+    new Info('Ship', 'About the biggest Ship'),
+    new Info('Plane', 'Its about the biggest Plane'),
+    new Info('Hide', '')
+  ];
+  text: string = '';
+
+  onSelect(item: Info)
+  {
+    this.text = item.desc;
+  }
+}
+class Info
+{
+  name: string;
+  desc: string;
+
+  constructor(name: string, desc: string)
+  {
+    this.name = name;
+    this.desc = desc;
+  }
 }
